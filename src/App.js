75229494f725762d977a9e872e7e675f0af4cd2b
ZHAR CHAIMAA <czhar@externe.gbp.ma>
2023-03-24 14:12:30 +0000

    tentative1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UploadFiles from "./components/upload-files.component";
+import FileUploadMultiple from "./components/uploadFileMultiple";
+import { useSelector,useDispatch } from 'react-redux';
+import { increment,decrement } from './action';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const counter = useSelector(state =>state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Counter {counter}</h1> 
+     <button onClick={()=>dispatch(increment(5))}>+</button>
+     <button onClick={()=>dispatch(decrement())}>-</button>
+     {isLogged? <h3>isLogged</h3>: 'is not logged'}
+     <div className="container" style={{width : "600px"}}>
+  <UploadFiles/>
+     </div>
+     <FileUploadMultiple/>
+   
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,59 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+import {Provider} from 'react-redux';
+//we can add redux devtools 
+const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+/*
+const increment = ()=>{
+  return{
+    type:'INCREMENT'
+  }
+}
+const decrement = ()=>{
+  return{
+    type:'DECREMENT'
+  }
+}
+const initialState=0;
+const counter=(state = 5,action)=>{
+  switch(action.type){
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
 
+
+}
+
+let store = createStore(counter);
+
+// display it in the console 
+
+store.subscribe(()=>console.log(store.getState()));
+
+
+//STORE ->global state-> va contenir 2 arguments => state and reducer
+
+//ACTION
+
+//REDUCER -> decrit comment vos actions transforment l'etat en autre etat => REDUCER verifie quelle action vous avez fait et en fonction de l'action cela va modifier le store 
+
+//DISPATCH =envoyer => dispatch an action  = quelle action a été envoyé et en fonction de type reducer va effectuer un traitement sur l'ete et va retourner un nouveau etat->comment executer une action => dispatch cette action au reducteur afin que nous puissions envoyer cette action au reducer et reducer va verifier qu'est ce qui va faire  et en suite le store va faire une mise à jour
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
